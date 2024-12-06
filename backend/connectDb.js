844d60ed2f2c9788f3b76c5c7264460d3e9126ab
MarvellousSam <marvel10cent@gmail.com>
2024-12-06 12:44:39 +0100

    bcrypt final

--- backend/connectDb.js
+++ backend/connectDb.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose')
 
 const connectDb=async()=>{
     try{
-        const db = await mongoose.connect(process.env.MONGO_URL)
+        const mongoUrl = process.env.MONGO_URL
+        const db = await mongoose.connect(mongoUrl)
         console.log(`Database connected to ${db.connection.host}`)
     }catch(error){
         console.log(error)
